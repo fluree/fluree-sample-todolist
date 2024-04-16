48ca15015ce3140d7caec7c7bd6c82057aac031f
Jack White <jwhite5672@gmail.com>
2024-04-16 12:42:10 -0400

    Added edit todo capability. Added icons for edit and delete buttons.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,4 @@
+import { XCircleIcon, PencilSquareIcon, CheckCircleIcon, TrashIcon } from '@heroicons/react/24/outline';
 import React, { useState } from 'react';
 
 interface TodoListProps {
@@ -12,6 +13,7 @@ type Todo = {
 
 const TodoList: React.FC<TodoListProps> = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [editing, setEditing] = useState<number>();
 
   const addTodo = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -31,12 +33,30 @@ const TodoList: React.FC<TodoListProps> = () => {
     setTodos(updatedTodos);
   };
 
+  // handle edit todo form submission and updates the todo label based on the input
+  const editTodo = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const editTodoForm = event.target as HTMLFormElement;
+    const input = editTodoForm.elements[0] as HTMLInputElement;
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === editing) {
+        return {
+          ...todo,
+          label: input.value
+        };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+    setEditing(undefined);
+  };
+  
   return (
     <main>
       <h1 className="sr-only">Todos</h1>
       <form className="add-todo-form" onSubmit={addTodo}>
         <label htmlFor="todo">Enter Task</label>
-        <input type="text" id="todo" name="todo" required />
+        <input className="add-input" type="text" id="todo" name="todo" required />
         <button className="add-button" type="submit">
           Add
         </button>
@@ -44,10 +64,30 @@ const TodoList: React.FC<TodoListProps> = () => {
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            {todo.label}
-            <button className="delete-button" type="button" onClick={() => deleteTodo(todo.id)}>
-              Delete
-            </button>
+            {editing !== todo.id && (<>
+              <span className={todo.isDone ? "todo-done" : ""}>{todo.label}</span>
+              <div className='todo-actions'>
+                <button className="edit-button" type="button" onClick={() => setEditing(todo.id)}>
+                  <PencilSquareIcon className="todo-icon" />
+                </button>
+                <button className="delete-button" type="button" onClick={() => deleteTodo(todo.id)}>
+                  <TrashIcon className="todo-icon" />
+                </button>
+              </div>
+            </>)}
+            {editing === todo.id && (
+              <form className="edit-todo-form" onSubmit={editTodo}>
+                <input className="edit-input" type="text" id="editTodo" name="editTodo" defaultValue={todo.label} required />
+                <div className='todo-actions'>
+                  <button className="edit-button" type="submit">
+                    <CheckCircleIcon className="todo-icon" />
+                  </button>
+                  <button className="cancel-button" type="button" onClick={() => setEditing(undefined)}>
+                    <XCircleIcon className="todo-icon" />
+                  </button>
+                </div>
+              </form>
+            )}
           </li>
         ))}
       </ul>
